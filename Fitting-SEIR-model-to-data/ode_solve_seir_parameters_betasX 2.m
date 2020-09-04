3e705640a844c949fb8c588e7adf78230a824ca7
ainebyrne <70653195+ainebyrne@users.noreply.github.com>
2020-09-04 08:02:26 +0100

    Add files via upload

---
+++ Fitting-SEIR-model-to-data/ode_solve_seir_parameters_betasX 2.m
@@ -0,0 +1,5 @@
+function penalty=ode_solve_seir_parameters_betasX(u,cases)
+
+[~,~,~,~,~,penalty]=ode_solve_seir_parameters_betas(u,cases);
+
+end
---
+++ Optimal-control-with-one-switch/ode_solve_seirX.m
@@ -0,0 +1,5 @@
+function penalty=ode_solve_seirX(u)
+
+[~,~,~,penalty]=ode_solve_seir(u);
+
+end
